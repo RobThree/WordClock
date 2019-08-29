6c9429fb96c850236f6563ff8417da913ba48f1a
RobThree <rob@devcorner.nl>
2019-08-29 02:20:42 +0200

    Cleanup / refactoring to separate files

---
+++ include/clock.h
@@ -0,0 +1,3 @@
+void TimestampToLEDS(long unixDateTime, bool useOffset);
+void ShowWords(int argcount, ...);
+void ClearLeds();
---
+++ src/wifi.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <wifi.h>
+
+#include "config.h"
+
+const char* wifi_ssid = WIFI_SSID;
+const char* wifi_pass = WIFI_PASSWD;
+
+void InitWiFi() {
+    Serial.printf("Connecting to %s...\n", wifi_ssid);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi_ssid, wifi_pass);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("Connection to %s Failed! Rebooting...\n", wifi_ssid);
+        delay(5000);
+        ESP.restart();
+    }
+    Serial.printf("Connected to %s\n", wifi_ssid);
+}
