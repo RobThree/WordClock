e5581de9f90a354ccd82f630955864a84f7cfb0f
Dennie Bastiaan <dennie@tellick.nl>
2020-03-17 21:52:52 +0100

    Animations!

---
+++ include/animation.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <display.h>
+
+class Animation
+{
+public:
+    static void SetAnimation(String url);
+    static void SetAnimationFrame(long time);
+private:
+};
+#endif
