RobThree/WordClock

commit 6b7cf64eb5374f1ad3137e279a703c74ec89cd31

Display class now handles logical -> physical mapping. Added convenience methods to address LEDs by row/col. Also the words are now stored more efficiently. Use (more) explicit data types. Serial speed can now be set in config.

src/main.cpp
@@ -13,7 +13,7 @@ long frameduration;
 
 void setup()
 {
-    Serial.begin(460800);
+    Serial.begin(SERIAL_SPEED);
     Serial.println("Booting...");
 
     pinMode(DISPLAY_LED_PIN, OUTPUT);
@@ -40,13 +40,13 @@ void setup()
 void loop()
 {
     // Get current time
-    long time = millis();
+    uint32_t time = millis();
 
     // Main loop
     OTA::Handle();
 
-    // comment Clock::TimestampToDisplay(...) and uncomment next line to switch to animation-mode
-    // of course this needs some sort of state switching, but for now this is it
+    // Comment Clock::TimestampToDisplay(...) and uncomment next line to switch to animation-mode
+    // Ofcourse this needs some sort of state switching, but for now this is it
     // a fun party trick is to leave them on both, I recommend matrix.bin for this
     // Animation::SetAnimationFrame(time);
 
@@ -56,7 +56,7 @@ void loop()
     
     // Determine at what time the next frame should be ready and how long we should
     // wait and then delay for that time.
-    long t_next = time + frameduration;
-    long t_wait = t_next - millis();
+    uint32_t t_next = time + frameduration;
+    uint32_t t_wait = t_next - millis();
     delay(t_wait >= 0 ? t_wait : 0);
 }

include/animation.h
@@ -7,7 +7,7 @@ class Animation
 {
 public:
     static void SetAnimation(String url);
-    static void SetAnimationFrame(long time);
+    static void SetAnimationFrame(uint32_t time);
 private:
 };
 #endif

include/clock.h
@@ -21,7 +21,8 @@ private:
         DIGIT_9,
         DIGIT_10,
         DIGIT_11,
-        ITIS,
+        IT,
+        IS,
         FIVE,
         TEN,
         TO,
@@ -32,10 +33,10 @@ private:
     };
 
 public:
-    static void TimestampToDisplay(long unixDateTime, bool useOffset);
+    static void TimestampToDisplay(uint32_t unixDateTime, bool useOffset);
 
 private:
-    static void ShowWords(int argcount, ...);
+    static void ShowWords(uint8_t argcount, ...);
 };
 
 #endif

include/display.h
@@ -7,10 +7,15 @@ class Display
 {
 public:
     static void Initialize();
-    static void SetLED(int index, CRGB color);
+    static void SetLED(uint8_t index, CRGB color);
+    static void SetLED(uint8_t row, uint8_t col, CRGB color);
+    static CRGB GetLED(uint8_t index);
+    static CRGB GetLED(uint8_t row, uint8_t col);
     static void Refresh();
 private:
     static void Clear();
-    static double GetBlinkValue(long time, double freq = .5, int min = 0, int max = 255);
+    static double GetBlinkValue(uint32_t time, double freq = .5, uint8_t min = 0, uint8_t max = 255);
+    static uint8_t MapLED(uint8_t index);
+    static bool CheckBounds(uint8_t index);
 };
 #endif

include/example_config.h
@@ -19,8 +19,11 @@
 
 // LED display properties
 #define DISPLAY_LED_PIN         D2
-#define DISPLAY_NUM_LEDS        115
+#define DISPLAY_ROWS            10
+#define DISPLAY_COLS            11
 #define DISPLAY_NUM_STATUSLEDS  5
+#define DISPLAY_NUM_LEDS_SCREEN DISPLAY_ROWS * DISPLAY_COLS
+#define DISPLAY_NUM_LEDS_TOTAL  DISPLAY_NUM_LEDS_SCREEN + DISPLAY_NUM_STATUSLEDS
 #define DISPLAY_LED_TYPE        WS2812B
 #define DISPLAY_COLOR_ORDER     GRB
 
@@ -40,4 +43,5 @@
 // Clock settings
 #define CLOCK_USEROUNDING   true    // Will round to the nearest 5 minutes instead of switching at exact time
 #define CLOCK_FPS           25      // Number of (desired) frames per second, no guarantees
+#define SERIAL_SPEED        460800
 #endif

src/animation.cpp
@@ -7,7 +7,7 @@
 uint16_t frameCount = 0;
 uint16_t currentFrame = 0;
 uint16_t frameTime = 1000;
-long lastTime = 0;
+uint32_t lastTime = 0;
 uint8_t data = 0;
 
 #define max(a,b) ((a)>(b)?(a):(b))
@@ -21,40 +21,34 @@ AnimationFrame frames[60];
 
 
 void Animation::SetAnimation(String url) {
-    // fixed frametime, for now
-    // right now the delay (16 bytes) isn't loaded and is included in the offset.
+    // Fixed frametime, for now
+    // Right now the delay (16 bytes) isn't loaded and is included in the offset.
     frameTime = 250;
 
     WiFiClient client;
     HTTPClient http;
     if (http.begin(client, url)) {
-        int httpCode = http.GET();
+        uint16_t httpCode = http.GET();
         if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_FOUND) {
             uint8_t buffer[http.getSize()];
             client.readBytes(buffer, sizeof(buffer));
 
-            // framecount, width, height and delay are 16 bytes
-            // little endian order in buffer!
+            // Framecount, width, height and delay are 16 bytes
+            // Little endian order in buffer!
             frameCount = (buffer[1] << 8) + buffer[0];
             uint16_t width = (buffer[3] << 8) + buffer[2];
             uint16_t height = (buffer[5] << 8) + buffer[4];
 
-            for (int i = 0; i < frameCount; i++) {
+            for (uint8_t i = 0; i < frameCount; i++) {
                 // offset = number of frames, times 110 pixels, times 3 bytes per pixel, plus six header bytes, plus two header bytes per frame
-                int offset = i * 110 * 3 + 6 + ((i + 1) * 2);
-                for (int y = 0; y < height; y++) {
-                    for (int x = 0; x < width; x++) {
-                        int pixel = (y * width + x) * 3 + offset;
+                uint16_t offset = i * 110 * 3 + 6 + ((i + 1) * 2);
+                for (uint8_t y = 0; y < height; y++) {
+                    for (uint8_t x = 0; x < width; x++) {
+                        uint16_t pixel = (y * width + x) * 3 + offset;
                         CRGB rgb;
                         rgb.setRGB(buffer[pixel + 2], buffer[pixel + 1], buffer[pixel]);
 
-                        // led strip direction is back and forth over rows
-                        // excluding the status leds, even rows are right to left, odd rows are left to right
-                        if (y % 2 == 0) {
-                            memcpy(&frames[i].pixels[(y * width) + width - 1 - x], &rgb, sizeof(rgb));
-                        } else{
-                            memcpy(&frames[i].pixels[y * width + x], &rgb, sizeof(rgb));
-                        }
+                        memcpy(&frames[i].pixels[y * width + x], &rgb, sizeof(rgb));
                     }
                 }
             }
@@ -62,20 +56,17 @@ void Animation::SetAnimation(String url) {
     }
 }
 
-void Animation::SetAnimationFrame(long time) {
-    // initialize lastTime
+void Animation::SetAnimationFrame(uint32_t time) {
+    // Initialize lastTime
     if (lastTime == 0) {
         lastTime = time;
         return;
     }
 
-    for (int i = 0; i < DISPLAY_NUM_LEDS - DISPLAY_NUM_STATUSLEDS; i++) {
-        //CRGB newPixel = frames[currentFrame].pixels[i];
-        //newPixel.setRGB(max(0, newPixel.r - 100), max(0, newPixel.g - 100), max(0, newPixel.b - 100));
-        Display::SetLED(i + DISPLAY_NUM_STATUSLEDS, frames[currentFrame].pixels[i]);
-    }
+    for (uint8_t i = 0; i < DISPLAY_NUM_LEDS_SCREEN; i++)
+        Display::SetLED(i, frames[currentFrame].pixels[i]);
 
-    // wait untill frametime is passed, then increase currentFrame
+    // Wait until frametime is passed, then increase currentFrame
     // TODO: handle overshoot when frameTime and frameRate are not compatible
     if (time - lastTime >= frameTime) { 
         if (currentFrame < frameCount - 1) {

src/clock.cpp
@@ -3,51 +3,52 @@
 #include <clock.h>
 #include <statusbar.h>
 
-// Make sure each entry ends with a null terminator (0x00)!
-byte(*WORDS[]) =
+// Words defined as (startindex, length)
+uint8_t WORDS[] =
 {
-    /*  0: Twelve  */ (byte[]){ 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x00 },
-    /*  1: One     */ (byte[]){ 0x2C, 0x2D, 0x2E, 0x00 },
-    /*  2: Two     */ (byte[]){ 0x3B, 0x3A, 0x39, 0x38, 0x00 },
-    /*  3: Three   */ (byte[]){ 0x34, 0x33, 0x32, 0x31, 0x00 },
-    /*  4: Four    */ (byte[]){ 0x3D, 0x3E, 0x3F, 0x40, 0x00 },
-    /*  5: Five    */ (byte[]){ 0x42, 0x43, 0x44, 0x45, 0x00 },
-    /*  6: Six     */ (byte[]){ 0x51, 0x50, 0x4F, 0x00 },
-    /*  7: Seven   */ (byte[]){ 0x4C, 0x4B, 0x4A, 0x49, 0x48, 0x00 },
-    /*  8: Eight   */ (byte[]){ 0x53, 0x54, 0x55, 0x56, 0x00 },
-    /*  9: Nine    */ (byte[]){ 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x00 },
-    /* 10: Ten     */ (byte[]){ 0x66, 0x65, 0x64, 0x63, 0x00 },
-    /* 11: Eleven  */ (byte[]){ 0x60, 0x5F, 0x5E, 0x00 },
-    /* 12: It is   */ (byte[]){ 0x0F, 0x0E, 0x0D, 0x0B, 0X0A, 0x00 },
-    /* 13: Five    */ (byte[]){ 0x08, 0x07, 0x06, 0x05, 0x00 },
-    /* 14: Ten     */ (byte[]){ 0x11, 0x12, 0x13, 0x14, 0x00 },
-    /* 15: To      */ (byte[]){ 0x25, 0x24, 0x23, 0x22, 0x00 },
-    /* 16: Past    */ (byte[]){ 0x1F, 0x1E, 0x1D, 0x1C, 0x00 },
-    /* 17: Quarter */ (byte[]){ 0x16, 0x17, 0x18, 0x19, 0x1A, 0x00 },
-    /* 18: Half    */ (byte[]){ 0x27, 0x28, 0x29, 0x2A, 0x00 },
-    /* 19: O'Clock */ (byte[]){ 0x70, 0x71, 0x72, 0x00 }
+    /*  0: Twelve  */ 0x63, 0x06,
+    /*  1: One     */ 0x27, 0x03,
+    /*  2: Two     */ 0x2C, 0x04,
+    /*  3: Three   */ 0x33, 0x04,
+    /*  4: Four    */ 0x38, 0x04,
+    /*  5: Five    */ 0x3D, 0x04,
+    /*  6: Six     */ 0x42, 0x03,
+    /*  7: Seven   */ 0x47, 0x05,
+    /*  8: Eight   */ 0x4E, 0x04,
+    /*  9: Nine    */ 0x53, 0x05,
+    /* 10: Ten     */ 0x59, 0x04,
+    /* 11: Eleven  */ 0x5F, 0x03,
+    /* 12: It      */ 0x00, 0x03,
+    /* 13: Is      */ 0x04, 0x02,
+    /* 14: Five    */ 0x07, 0x04,
+    /* 15: Ten     */ 0x0C, 0x04,
+    /* 16: To      */ 0x16, 0x04,
+    /* 17: Past    */ 0x1C, 0x04,
+    /* 18: Quarter */ 0x11, 0x05,
+    /* 19: Half    */ 0x22, 0x04,
+    /* 20: O'Clock */ 0x6B, 0x03,
 };
 
-void Clock::TimestampToDisplay(long unixDateTime, bool useOffset = false)
+void Clock::TimestampToDisplay(uint32_t unixDateTime, bool useOffset = false)
 {
     // Strip date part, keep time, apply offset when desired
-    int time = (unixDateTime + (useOffset ? 150 : 0)) % 86400;
-    int hour = time / 3600;       // Determine the hour
-    int part = time % 3600 / 300; // Determine which part of the hour (truncated to 5 minutes)
+    uint16_t time = (unixDateTime + (useOffset ? 150 : 0)) % 86400;
+    uint8_t hour = time / 3600;       // Determine the hour
+    uint8_t part = time % 3600 / 300; // Determine which part of the hour (truncated to 5 minutes)
 
-    ShowWords(1, ITIS);
+    ShowWords(2, IT, IS);
     switch (part)
     {
-        case 0: break;
-        case 1: ShowWords(2, FIVE, PAST); break;
-        case 2: ShowWords(2, TEN, PAST); break;
-        case 3: ShowWords(2, QUARTER, PAST); break;
-        case 4: ShowWords(3, TEN, TO, HALF); break;
-        case 5: ShowWords(3, FIVE, TO, HALF); break;
-        case 6: ShowWords(1, HALF); break;
-        case 7: ShowWords(3, FIVE, PAST, HALF); break;
-        case 8: ShowWords(3, TEN, PAST, HALF); break;
-        case 9: ShowWords(2, QUARTER, TO); break;
+        case 0:  break;
+        case 1:  ShowWords(2, FIVE, PAST); break;
+        case 2:  ShowWords(2, TEN, PAST); break;
+        case 3:  ShowWords(2, QUARTER, PAST); break;
+        case 4:  ShowWords(3, TEN, TO, HALF); break;
+        case 5:  ShowWords(3, FIVE, TO, HALF); break;
+        case 6:  ShowWords(1, HALF); break;
+        case 7:  ShowWords(3, FIVE, PAST, HALF); break;
+        case 8:  ShowWords(3, TEN, PAST, HALF); break;
+        case 9:  ShowWords(2, QUARTER, TO); break;
         case 10: ShowWords(2, TEN, TO); break;
         case 11: ShowWords(2, FIVE, TO); break;
     }
@@ -60,17 +61,19 @@ void Clock::TimestampToDisplay(long unixDateTime, bool useOffset = false)
         ShowWords(1, HOUR);
 }
 
-void Clock::ShowWords(int argcount, ...)
+void Clock::ShowWords(uint8_t argcount, ...)
 {
     va_list arguments;
     va_start(arguments, argcount);
     // Iterate the words
-    for (int i = 0; i < argcount; i++)
+    for (uint8_t i = 0; i < argcount; i++)
     {
-        int wordindex = va_arg(arguments, int);
+        uint8_t wordindex = va_arg(arguments, int) * 2;
+        uint8_t wordstart = WORDS[wordindex];
+        uint8_t wordlen = WORDS[wordindex + 1];
 
-        for (byte *c = WORDS[wordindex]; *c != 0; c++)
-            Display::SetLED(*c, CRGB::White);
+        for (uint8_t c = 0; c < wordlen; c++)
+            Display::SetLED(wordstart + c, CRGB::White);
         
         va_end(arguments);
     }

commit 73b66180cf6e1d074adbfc227ac22478f63f8f7d

Fixed subtle bug that got introduced in 6b7cf64 by switching from signed to unsigned which caused a huge delays because of something that previously would have been a negative number now rolling around / overflowing. Also snuck in new animation ;-)

src/main.cpp
@@ -12,7 +12,7 @@
 #include <rainbow.h>
 #include <gradient.h>
 
-long frameduration;
+uint32_t frameduration;
 
 void setup()
 {
@@ -39,6 +39,7 @@ void setup()
     // Gradient::Initialize(CRGB(255, 0, 0), CRGB(0, 255, 0));
     
     // Example animations
+    // Animation::Initialize("http://xs4any.nl/marios/fire.bin");
     // Animation::Initialize("http://xs4any.nl/marios/spiral.bin");
     // Animation::Initialize("http://xs4any.nl/marios/testscreen.bin");
     // Animation::Initialize("http://xs4any.nl/marios/mario.bin");
@@ -69,10 +70,10 @@ void loop()
     Clock::Handle(info);
     
     Display::Refresh();
-    
+
     // Determine at what time the next frame should be ready and how long we should
     // wait and then delay for that time.
     uint32_t t_next = info.uptime + frameduration;
     uint32_t t_wait = t_next - millis();
-    delay(t_wait >= 0 ? t_wait : 0);
+    delay(t_wait <= frameduration ? t_wait : 0);
 }